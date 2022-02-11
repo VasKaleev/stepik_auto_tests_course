bf1c6a5d99604f1e8603f7c3d0c8d8a0a453f9d8
Vasily Kaleev <kaleev.fam@mail.ru>
2022-02-11 09:34:39 +0300

    Добавил комментарии

--- task2_4_8.py
+++ task2_4_8.py
@@ -8,27 +8,19 @@ browser = webdriver.Chrome()
 
 browser.get("http://suninjuly.github.io/explicit_wait2.html")
 
-# говорим Selenium проверять в течение 5 секунд, пока кнопка не станет кликабельной
-# button = WebDriverWait(browser, 12).until(
-        # EC.element_to_be_clickable((By.ID, "verify"))
-    # )
 WebDriverWait(browser, 12).until(EC.text_to_be_present_in_element((By.ID, "price"), "$100"))
 btn = browser.find_element_by_id("book") 
 btn.click()
 
-# Ваш код, который заполняет обязательные поля    
+#Код, который заполняет обязательные поля    
 x_element = browser.find_element_by_id("input_value")
 x = x_element.text
-# y = str(math.log(abs(12*math.sin(int(x)))))
        
 print(x_element)
      
 input1 = browser.find_element_by_id("answer")
 input1.send_keys(str(math.log(abs(12*math.sin(int(x))))))
     
-# button = browser.find_element_by_css_selector("solve")
-# button.click()
-
 button = WebDriverWait(browser, 5).until(
         EC.element_to_be_clickable((By.ID, "solve"))
     )
